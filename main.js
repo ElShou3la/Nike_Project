7011a1c351b0ce8364806bb21d5705cf2daea530
ahmed ashraf <ahmedashraf122006ah@gmail.com>
2023-06-19 10:56:15 -0700

    i push

---
+++ main.js
@@ -0,0 +1,11 @@
+function changeImage(fileName){
+let img  = document.getElementById("image")
+img.setAttribute("src" , fileName)
+}
+let ule = document.getElementById("ule")
+
+
+function header(){
+    ule.style.display = "flex"
+}
+
